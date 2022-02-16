30d32b55841ecb880f35c5dc3f487721a84b7480
sm970309 <sm970309@gmail.com>
2022-02-16 17:08:40 +0900

    Add new file

---
+++ WEBSOCKET/realtime_data_with_pybithumb.py
@@ -0,0 +1,46 @@
+from pybithumb import WebSocketManager
+import sys
+import time
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QIcon
+
+class Worker(QThread):
+    def __init__(self):
+        super().__init__()
+
+    recv = pyqtSignal(str)
+    def run(self):
+        wm = WebSocketManager("ticker", ["BTC_KRW"])
+        while True:
+            data = wm.get()
+            self.recv.emit(data['content']['closePrice'])
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        label = QLabel("BTC",self)
+        label.move(20,20)
+
+        self.price = QTextEdit("",self)
+        self.price.move(80,20)
+        self.price.resize(100,25)
+
+        button = QPushButton("Start", self)
+        button.move(20,50)
+        button.clicked.connect(self.click_btn)
+        self.th = Worker()
+        self.th.recv.connect(self.receive_msg)
+    def click_btn(self):
+        self.th.start()
+
+    @pyqtSlot(str)
+    def receive_msg(self,msg):
+        self.price.setText(msg)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mywindow = MyWindow()
+    mywindow.show()
+    app.exec_()
